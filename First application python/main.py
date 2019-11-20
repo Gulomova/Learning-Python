81d269626a16c652a5051752c00d0ffc6fa4dd12
Fatima Gulomova <fatimagulomova@gmail.com>
2019-11-20 15:18:34 +0200

    creating new project First application python

---
+++ First application python/main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+class Main (tk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = Main (root)
+    app.pack()
+    root.title("First application python")
+    root.geometry("650x450+300+200")
+    root.resizable (False, False)
+    root.mainloop()
+
+
